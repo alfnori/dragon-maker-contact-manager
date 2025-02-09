c87bf0434d289c4e7aa0a3e0baf849155762a7d8
Adolfo Luiz <alfnori@gmail.com>
2025-02-09 16:58:19 -0300

    chore: add link to logo

--- src/components/Common/Logo.tsx
+++ src/components/Common/Logo.tsx
@@ -4,9 +4,10 @@ import { useNavigate } from 'react-router';
 
 interface LogoProps {
   size?: number;
+  link?: string;
 }
 
-export const Logo: React.FC<LogoProps> = ({ size = 84 }) => {
+export const Logo: React.FC<LogoProps> = ({ size = 84, link = '/' }) => {
   const dragonRoar = keyframes`
   0% {
     transform: scale(0.9);
@@ -32,7 +33,7 @@ export const Logo: React.FC<LogoProps> = ({ size = 84 }) => {
   const navigate = useNavigate();
 
   return (
-    <IconButton onClick={() => navigate('/')}>
+    <IconButton onClick={() => navigate(link)}>
       <Avatar
         alt="Dragon Maker"
         src="/dragon-maker.png"
